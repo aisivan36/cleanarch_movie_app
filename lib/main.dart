106516129fce560020539cbf3c61437aacc71d5e
ivan-boginski <aisivan36@gmail.com>
2022-08-11 07:04:52 +0700

    nothing changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,19 @@ import 'package:provider/provider.dart';
 import 'injection.dart' as di;
 
 void main() {
+  //  TODO declare an Injection here
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [],
+      providers: [
+        /// General things
+      ],
     );
   }
 }
