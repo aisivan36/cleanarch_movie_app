616ba045a64a6349c15937072a1e22c9736cc594
ivan-boginski <aisivan36@gmail.com>
2022-08-16 09:45:12 +0700

    injection

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,1 +1,12 @@
+import 'package:cleanarch_movie_app/core/presentation/provider/home_notifier.dart';
+import 'package:get_it/get_it.dart';
 
+final locator = GetIt.instance;
+
+void init() {
+  /// TODO Movie Bloc Search and TV
+  ///
+
+  // Provider
+  locator.registerFactory(() => HomeNotifier());
+}
