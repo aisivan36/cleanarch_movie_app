748612572c97b05d3d59e508bb3229767f84bd73
ivan-boginski <aisivan36@gmail.com>
2022-09-01 20:02:41 +0700

    write up "top rated moives screen" sirah bileng break up little bit

---
+++ lib/movie/presentation/pages/top_rated_movies_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TopRatedMoviesPage extends StatefulWidget {
+  static const String routeName = '/top-rated-movies';
+
+  const TopRatedMoviesPage({super.key});
+
+  @override
+  State<TopRatedMoviesPage> createState() => _TopRatedMoviesPageState();
+}
+
+class _TopRatedMoviesPageState extends State<TopRatedMoviesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
