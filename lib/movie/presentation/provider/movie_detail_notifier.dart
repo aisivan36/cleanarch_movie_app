76a3d9751a10d726fdfb96852c3343a720a444ed
aisivan36 <aisivan36@gmail.com>
2022-09-05 08:16:59 +0700

    todo watch this in case if an error occurred

--- lib/movie/presentation/provider/movie_detail_notifier.dart
+++ lib/movie/presentation/provider/movie_detail_notifier.dart
@@ -1,4 +1,5 @@
 import 'package:cleanarch_movie_app/core/utils/state_enum.dart';
+import 'package:cleanarch_movie_app/movie/domain/entities/movie.dart';
 import 'package:cleanarch_movie_app/movie/domain/entities/movie_detail.dart';
 import 'package:cleanarch_movie_app/movie/domain/usecases/get_movie_detail.dart';
 import 'package:cleanarch_movie_app/movie/domain/usecases/get_movie_recommendations.dart';
@@ -31,4 +32,78 @@ class MovieDetailNotifier extends ChangeNotifier {
 
   RequestState _movieState = RequestState.empty;
   RequestState get movieState => _movieState;
+
+  List<Movie> _recommendations = [];
+  List<Movie> get recommendations => _recommendations;
+
+  RequestState _recommendationsState = RequestState.empty;
+  RequestState get recommendationsState => _recommendationsState;
+
+  String _message = '';
+  String get message => _message;
+
+  bool _isAddedToWatchlist = false;
+  bool get isAddedToWatchlist => _isAddedToWatchlist;
+
+  String _watchlistMessage = '';
+  String get watchlistMessage => _watchlistMessage;
+
+  Future<void> fetchMovieDetail(int id) async {
+    _movieState = RequestState.loading;
+    notifyListeners();
+
+    final detailResult = await getMovieDetail.execute(id);
+    final recommendationResult = await getMovieRecommendations.execute(id);
+
+    detailResult.fold((failure) {
+      _movieState = RequestState.error;
+      _message = failure.message;
+      notifyListeners();
+    }, (movie) {
+      _recommendationsState = RequestState.loading;
+      _movie = movie;
+      notifyListeners();
+      recommendationResult.fold((failure) {
+        _recommendationsState = RequestState.error;
+        _message = failure.message;
+      }, (movies) {
+        _recommendationsState = RequestState.loaded;
+        _recommendations = movies;
+      });
+      _movieState = RequestState.loaded;
+      notifyListeners();
+    });
+  }
+
+  Future<void> addToWatchlist(MovieDetail movie) async {
+    final result = await saveWatchlist.execute(movie);
+
+    /// TODO watch this,,, I removed "await" in case something went wrong head to fix it
+    result.fold((failure) {
+      _watchlistMessage = failure.message;
+    }, (successMessage) {
+      _watchlistMessage = successMessage;
+    });
+    await loadWatchlistStatus(movie.id);
+  }
+
+  Future<void> removeFromWatchlist(MovieDetail movie) async {
+    final result = await removeWatchlist.execute(movie);
+
+    /// TODO watch this,,, I removed "await" in case something went wrong head to fix it
+    result.fold((failure) {
+      _watchlistMessage = failure.message;
+    }, (successMessage) {
+      _watchlistMessage = successMessage;
+    });
+
+    await loadWatchlistStatus(movie.id);
+  }
+
+  Future<void> loadWatchlistStatus(int id) async {
+    final result = await getWatchListStatus.execute(id);
+
+    _isAddedToWatchlist = result;
+    notifyListeners();
+  }
 }
