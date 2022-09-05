c3aeda9919eab221b9e9d6aaed4048299587cd2b
aisivan36 <aisivan36@gmail.com>
2022-09-05 09:00:15 +0700

    watchlist movie notifier

---
+++ lib/movie/presentation/provider/watchlist_movie_notifier.dart
@@ -0,0 +1,36 @@
+import 'package:cleanarch_movie_app/core/utils/state_enum.dart';
+import 'package:cleanarch_movie_app/movie/domain/entities/movie.dart';
+import 'package:cleanarch_movie_app/movie/domain/usecases/get_watchlist_movies.dart';
+import 'package:flutter/cupertino.dart';
+
+class WatchlistMovieNotifier extends ChangeNotifier {
+  final GetWatchlistMovies getWatchlistMovies;
+
+  WatchlistMovieNotifier({required this.getWatchlistMovies});
+
+  var _watchlistMovies = <Movie>[];
+  List<Movie> get watchlistMovies => _watchlistMovies;
+
+  var _watchlistState = RequestState.empty;
+  RequestState get watchlisState => _watchlistState;
+
+  String _message = '';
+  String get message => _message;
+
+  Future<void> fetchWatchlistMovies() async {
+    _watchlistState = RequestState.loading;
+    notifyListeners();
+
+    final result = await getWatchlistMovies.execute();
+
+    result.fold((failure) {
+      _watchlistState = RequestState.error;
+      _message = failure.message;
+      notifyListeners();
+    }, (moviesData) {
+      _watchlistState = RequestState.loaded;
+      _watchlistMovies = moviesData;
+      notifyListeners();
+    });
+  }
+}
