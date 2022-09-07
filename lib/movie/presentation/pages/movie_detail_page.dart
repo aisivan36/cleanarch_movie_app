4cf26b1275878dc124f6fcd628c2c84d76e6f934
aisivan36 <aisivan36@gmail.com>
2022-09-07 21:43:43 +0700

    wrote

--- lib/movie/presentation/pages/movie_detail_page.dart
+++ lib/movie/presentation/pages/movie_detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:cleanarch_movie_app/core/utils/state_enum.dart';
 import 'package:cleanarch_movie_app/movie/domain/entities/movie.dart';
 import 'package:cleanarch_movie_app/movie/domain/entities/movie_detail.dart';
 import 'package:cleanarch_movie_app/movie/presentation/provider/movie_detail_notifier.dart';
@@ -23,17 +24,38 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
   void initState() {
     super.initState();
 
-    Future.microtask(() {
-      Provider.of<MovieDetailNotifier>(context, listen: false)
-          .fetchMovieDetail(widget.id);
-      Provider.of<MovieDetailNotifier>(context, listen: false)
-          .loadWatchlistStatus(widget.id);
-    });
+    Future.microtask(
+      () {
+        Provider.of<MovieDetailNotifier>(context, listen: false)
+            .fetchMovieDetail(widget.id);
+        Provider.of<MovieDetailNotifier>(context, listen: false)
+            .loadWatchlistStatus(widget.id);
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Consumer<MovieDetailNotifier>(
+        builder: (context, value, child) {
+          if (value.movieState == RequestState.loading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (value.movieState == RequestState.loaded) {
+            final movie = value.movie;
+            return MovieDetailContent(
+              movie: movie,
+              recommendations: value.recommendations,
+              isAddedToWatchlist: value.isAddedToWatchlist,
+            );
+          } else {
+            return Text(value.message);
+          }
+        },
+      ),
+    );
   }
 }
 
