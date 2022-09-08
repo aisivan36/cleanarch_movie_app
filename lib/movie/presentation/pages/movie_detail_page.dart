c82f72cccaa8219ad5844e597d5162be72e926a9
aisivan36 <aisivan36@gmail.com>
2022-09-08 22:10:54 +0700

    fixed the broken things

--- lib/movie/presentation/pages/movie_detail_page.dart
+++ lib/movie/presentation/pages/movie_detail_page.dart
@@ -1,9 +1,16 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cleanarch_movie_app/core/styles/text_styles.dart';
 import 'package:cleanarch_movie_app/core/utils/state_enum.dart';
+import 'package:cleanarch_movie_app/core/utils/urls.dart';
+import 'package:cleanarch_movie_app/movie/domain/entities/genre.dart';
 import 'package:cleanarch_movie_app/movie/domain/entities/movie.dart';
 import 'package:cleanarch_movie_app/movie/domain/entities/movie_detail.dart';
 import 'package:cleanarch_movie_app/movie/presentation/provider/movie_detail_notifier.dart';
+import 'package:cleanarch_movie_app/movie/presentation/widgets/minimal_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shimmer/shimmer.dart';
 
 class MovieDetailPage extends StatefulWidget {
   static const String routeName = '/movie-detail';
@@ -59,7 +66,10 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
   }
 }
 
-class MovieDetailContent extends StatelessWidget {
+/// Using stateful is only for checking the BuildContext that is mounted in the [ElevatedButton] Function
+///
+/// TODO Probably in the future this is gonna be fixed and have to check frequently
+class MovieDetailContent extends StatefulWidget {
   final MovieDetail movie;
   final List<Movie> recommendations;
   final bool isAddedToWatchlist;
@@ -72,7 +82,349 @@ class MovieDetailContent extends StatelessWidget {
   });
 
   @override
+  State<MovieDetailContent> createState() => _MovieDetailContentState();
+}
+
+class _MovieDetailContentState extends State<MovieDetailContent> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomScrollView(
+      key: const Key('movieDetailScrollView'),
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: 250.0,
+          flexibleSpace: FlexibleSpaceBar(
+            background: FadeIn(
+              duration: const Duration(milliseconds: 500),
+              child: ShaderMask(
+                shaderCallback: (bounds) => const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.black,
+                    Colors.black,
+                    Colors.transparent,
+                  ],
+                  stops: [0.0, 0.5, 1.0, 1.0],
+                ).createShader(
+                  Rect.fromLTRB(
+                    0.0,
+                    0.0,
+                    bounds.width,
+                    bounds.height,
+                  ),
+                ),
+                blendMode: BlendMode.dstIn,
+                child: CachedNetworkImage(
+                  width: MediaQuery.of(context).size.width,
+                  imageUrl: Urls.imageUrl(widget.movie.backdropPath!),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: FadeInUp(
+            from: 50,
+            duration: const Duration(seconds: 2),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.movie.title,
+                    style: kHeading5.copyWith(
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[800],
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        child: Text(
+                          widget.movie.releaseDate
+                              .split('-')
+                              .reversed
+                              .join('/'),
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16.0),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 20.0,
+                          ),
+                          const SizedBox(width: 4.0),
+                          Text(
+                            (widget.movie.voteAverage / 2).toStringAsFixed(1),
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 1.2,
+                            ),
+                          ),
+                          const SizedBox(width: 4.0),
+                          Text(
+                            '(${widget.movie.voteAverage})',
+                            style: const TextStyle(
+                              fontSize: 9.0,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 1.2,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(width: 16.0),
+                      Text(
+                        _showDuration(widget.movie.runtime),
+                        style: const TextStyle(
+                          color: Colors.white70,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    key: const Key('movieToWatchlist'),
+                    onPressed: () async {
+                      if (!widget.isAddedToWatchlist) {
+                        await Provider.of<MovieDetailNotifier>(context,
+                                listen: false)
+                            .addToWatchlist(widget.movie);
+                      } else {
+                        await context
+                            .read<MovieDetailNotifier>()
+                            .removeFromWatchlist(widget.movie);
+                      }
+
+                      /// Using StatefulWidget for only using this statement to be checked before its mounted
+                      /// TODO Probably in the future this is gonna be fixed and have to check frequently
+                      if (!mounted) return;
+                      final message =
+                          context.read<MovieDetailNotifier>().watchlistMessage;
+
+                      if (message ==
+                              MovieDetailNotifier.watchlistAddSuccessMessage ||
+                          message ==
+                              MovieDetailNotifier
+                                  .watchlistRemoveSuccessMessage) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(message),
+                          ),
+                        );
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            content: Text(message),
+                          ),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: widget.isAddedToWatchlist
+                          ? Colors.grey[850]
+                          : Colors.white,
+                      minimumSize: Size(
+                        MediaQuery.of(context).size.width,
+                        42.0,
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        widget.isAddedToWatchlist
+                            ? const Icon(Icons.check, color: Colors.white)
+                            : const Icon(
+                                Icons.add,
+                                color: Colors.black,
+                              ),
+                        const SizedBox(width: 16.0),
+                        Text(
+                          widget.isAddedToWatchlist
+                              ? 'Added to watchlist'
+                              : 'Add to watchlist',
+                          style: TextStyle(
+                            color: widget.isAddedToWatchlist
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  Text(
+                    widget.movie.overview,
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    'Genre: ${_showGenres(widget.movie.genres)}',
+                    style: const TextStyle(
+                      color: Colors.white70,
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 24.0),
+          sliver: SliverToBoxAdapter(
+            child: FadeInUp(
+              from: 30,
+              duration: const Duration(milliseconds: 500),
+              child: Text(
+                'More Like This'.toUpperCase(),
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 1.2,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SliverPadding(
+          padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 24.0),
+          sliver: _ShowRecommendations(),
+        ),
+      ],
+    );
+  }
+
+  String _showGenres(List<Genre> genres) {
+    String result = '';
+
+    for (var genre in genres) {
+      result += '${genre.name}, ';
+    }
+
+    if (result.isEmpty) return result;
+
+    return result.substring(0, result.length - 2);
+  }
+
+  _showDuration(int runtime) {
+    final int hours = runtime ~/ 60;
+    final int minutes = runtime % 60;
+
+    if (hours > 0) {
+      return '${hours}h ${minutes}m';
+    } else {
+      return '${minutes}m';
+    }
+  }
+}
+
+class _ShowRecommendations extends StatelessWidget {
+  const _ShowRecommendations();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MovieDetailNotifier>(
+      builder: (context, value, child) {
+        if (value.recommendationsState == RequestState.loaded) {
+          return SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final recommendation = value.recommendations[index];
+                return FadeInUp(
+                  from: 20,
+                  duration: const Duration(milliseconds: 500),
+                  child: GestureDetector(
+                    onTap: () => showModalBottomSheet(
+                      context: context,
+                      builder: (context) =>
+                          MinimalDetail(movie: recommendation),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          topRight: Radius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(4.0),
+                      ),
+                      child: CachedNetworkImage(
+                        imageUrl: Urls.imageUrl(recommendation.posterPath!),
+                        placeholder: (context, url) => Shimmer.fromColors(
+                          baseColor: Colors.grey[850]!,
+                          highlightColor: Colors.grey[800]!,
+                          child: Container(
+                            height: 170.0,
+                            width: 120.0,
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                        ),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                        height: 180.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                );
+              },
+              childCount: value.recommendations.length,
+            ),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+              childAspectRatio: 0.7,
+              crossAxisCount:
+                  MediaQuery.of(context).orientation == Orientation.portrait
+                      ? 3
+                      : 4,
+            ),
+          );
+        } else if (value.recommendationsState == RequestState.error) {
+          return SliverToBoxAdapter(
+            child: Center(
+              child: Text(value.message),
+            ),
+          );
+        } else {
+          return const SliverToBoxAdapter(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
   }
 }
